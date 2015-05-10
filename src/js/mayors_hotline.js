9e2f6573380cecc66ce189a73be871a58f6f5a97
Ammar El Seed <ammar.elseed@gmail.com>
2015-05-10 11:52:17 -0400

    Getting there..

--- src/js/mayors_hotline.js
+++ src/js/mayors_hotline.js
@@ -1,24 +1,15 @@
 'use strict';
 
-var dateChart = dc.lineChart("#date-chart");
-var hourChart = dc.barChart("#hour-chart");
-var dayChart = dc.rowChart("#day-chart");
-var sourceChart = dc.rowChart("#source-chart");
+// Create charts and assign ids to them
 var statusChart = dc.rowChart("#status-chart");
-var neighborhoodChart = dc.rowChart("#neighborhood-chart");
-var reasonChart = dc.rowChart("#reason-chart");
+var serviceTypeChart = dc.rowChart("#serviceType-chart");
 var openDaysChart = dc.rowChart("#opendays-chart");
 var dataTable = dc.dataTable("#data-table")
 var dataCount = dc.dataCount('.data-count');
 
 var allCharts = [
-  {chart: dateChart, id: "#date-chart"},
-  {chart: hourChart, id: "#hour-chart"},
-  {chart: dayChart,  id: "#day-chart"},
-  {chart: sourceChart, id: "#source-chart"},
   {chart: statusChart, id: "#status-chart"},
-  {chart: neighborhoodChart, id: "#neighborhood-chart"},
-  {chart: reasonChart, id: "#reason-chart"},
+  {chart: serviceTypeChart, id: "#serviceType-chart"},
   {chart: openDaysChart, id: "#opendays-chart"}
 ];
 
@@ -28,8 +19,8 @@ var singleColor = ["#1a8bba"];
 var smallIcon = L.divIcon({className: "small-div-marker"});
 var mapClustersLayer = L.markerClusterGroup({maxClusterRadius: 50});
 var map = L.map('map', {
-  center: [42.313, -71.099],
-  zoom: 11,
+  center: [42.393,-71.104],
+  zoom: 12,
   maxZoom: 18,
   layers: [mapClustersLayer]
 });
@@ -44,17 +35,27 @@ var onFiltered = function(chart, filter) {
   updateMap(locations.top(Infinity));
 };
 
+function isNumber(n) {
+  return n != null && !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function validCoordinates(d) {
+  return d["Date "] != null && d.g.latitude != undefined && 
+    d.g.longitude != undefined &&
+    isNumber(d.g.latitude) && 
+    isNumber(d.g.logitude)
+}
+
 var updateMap = function(locs) {
   mapClustersLayer.clearLayers();
   var markers = locs.map(function(item){
     if( item.g.latitude!=null && item.g.latitude!=undefined) {
+
       return L.marker([item.g.latitude, item.g.longitude],
         {icon: smallIcon}).bindPopup(
-          item.t + 
-          "<br/>" + item.l +
-          "<br/><strong>Enquiry ID:</strong>" + item.id + 
-          "<br/><strong>Opened: </strong>" + item.d + 
-          "<br/><strong>Status: </strong>" + item.a
+          "<br/><strong>Address: </strong>" + item.Total_Location + 
+          "<br/><strong>Days Open: </strong>" + item.days_open+ 
+          "<br/><strong>Status: </strong>" + item.current_status
         );
     }
   });
@@ -65,179 +66,120 @@ var today = new Date();
 var thirty_days_ago = d3.time.day(new Date(today.getTime() - 30*24*60*60*1000));
 var tda_date = thirty_days_ago.toISOString().substring(0,10);
 
+var data;
+function handleFiles(fileList) {
+  var selectedFile = fileList[0]
+  Papa.parse(selectedFile,
+  {
+    encoding:'utf8', 
+    header:true,
+    delimiter: ",",
+    complete: 
+      function(results, file) {
+      data = results.data
 
-var boston_data_url = "https://data.cityofboston.gov/resource/awu8-dc52.csv?" + 
-  "$$app_token=bjp8KrRvAPtuf809u1UXnI0Z8&" + /* Socrata API app token */
-  "$limit=50000&" +
-  /* Renaming the columns to single-char-names helps reduce the payload,
-      as does selecting only the columns we use. */
-  "$select=case_enquiry_id as id, " +
-  "open_dt as d, " +
-  "closed_dt as c, " +
-  "closure_reason as c_exp," +
-  "source as s, " +
-  "case_status as a, " +
-  "neighborhood as n, " +
-  "geocoded_location as g, " +
-  "location as l, " +
-  "case_title as t, " + 
-  "reason as r&" +
-  "$where=open_dt>'" + tda_date + "'";
+      var dateFormat = d3.time.format("%m/%d/%Y");
+      data.forEach(function(d) {
 
-d3.csv(boston_data_url, function(err, data) {
-  //var dateFormat = d3.time.format("%Y-%m-%dT%H:%M:%S");
-  var dateFormat = d3.time.format("%m/%d/%Y %H:%M:%S %p");
-  data.forEach(function(d) {
-    d.date_opened = dateFormat.parse(d.d);
-    d.date_closed = (d.c !== "") ? dateFormat.parse(d.c) : today;
-    var lat = d.g.split(',')[0].slice(1);
-    var lon = d.g.split(',')[1].slice(0,-1);
-    d.n = (d.n !== "") ? d.n : "Not reported";
-    d.g = { latitude: lat, longitude: lon };
-    d.time_to_close = Math.round((d.date_closed - d.date_opened)/1000/60/60/24);
-  });
+        if ( !d["Date "] )
+          //skip processing; 
+          //this happens at the end of the file
+          return; 
 
-  var index = crossfilter(data);
-  var all = index.groupAll();
+        d.date_opened = dateFormat.parse(d["Date "])
+        d.days_open = Number(d["Days Open"])
+        d.days_late = Number(d["Days Late"])
+        d.current_status = (d.Closed == "Y" ? "Closed":"Open")
 
-  var sources = index.dimension( function(d) { return d.s; });
-  var open_dates = index.dimension( function(d) { return d3.time.hour(d.date_opened); } );
-  var open_hours = index.dimension( function(d) { return d.date_opened.getHours()+1; } );
-  var open_days = index.dimension( function(d) { 
-    var day = d.date_opened.getDay();
-    var name = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    return day + '.' + name[day];
-  });
-  var status = index.dimension( function(d) { return d.a; } );
-  var neighborhoods = index.dimension( function(d) { return d.n; } );
-  var reasons = index.dimension( function(d) { return d.r; } ); 
-  locations = index.dimension( function(d) { return d.g; });
-  var days_open = index.dimension( function(d) { return d.time_to_close; });
+        var lat = Number(d.GeoLocation_Lat_Long.split(',')[0])
+        var lon = Number(d.GeoLocation_Lat_Long.split(',')[1])
+        d.g = { latitude: lat, longitude: lon };
+        //d.time_to_close = Math.round((d.date_closed - d.date_opened)/1000/60/60/24);
+      });
 
-  dataCount
-    .dimension(index)
-    .group(all);
+      //remove last element because it has null information
+      data.pop();
 
-  dateChart
-    .width($('#date-chart').innerWidth()-30)
-    .height(200)
-    .margins({top: 10, left:30, right: 10, bottom:20})
-    .x(d3.time.scale().domain([thirty_days_ago, today]))
-    .colors(singleColor)
-    .dimension(open_dates)
-    .group(open_dates.group())
-    .renderArea(true)
-    .elasticY(true)
-    .yAxis().ticks(6);
-  dateChart.on("postRedraw", onFiltered);
+      var index = crossfilter(data);
+      var all = index.groupAll();
 
-  hourChart
-    .width($('#hour-chart').innerWidth()-30)
-    .height(250)
-    .margins({top: 10, left:35, right: 10, bottom:20})
-    .x(d3.scale.linear().domain([1,24]))
-    .colors(singleColor)
-    .dimension(open_hours)
-    .group(open_hours.group())
-    .gap(1)
-    .elasticY(true);
+      //var open_dates = index.dimension( function(d) { return d3.time.day(d.date_opened); } );
+      var open_dates = index.dimension( function(d) { return d.date_opened; } );
+      var are_closed = index.dimension( function(d) { return d.current_status; } );
+      var service_type = index.dimension( function(d) { return d["Service Type "]; } );
 
-  dayChart
-    .width($('#day-chart').innerWidth()-30)
-    .height(183)
-    .margins({top: 10, left:5, right: 10, bottom:-1})
-    .label( function(i) { return i.key.split('.')[1]; })
-    .title( function(i) { return i.key.split('.')[1] + ': ' + i.value; })
-    .colors(singleColor)
-    .dimension(open_days)
-    .group(open_days.group())
-    .elasticX(true)
-    .gap(1)
-    .xAxis().ticks(0);
+      locations = index.dimension( function(d) { return d.g; });
+      var days_open = index.dimension( function(d) { return d.days_open; });
 
-  statusChart
-    .width($('#status-chart').innerWidth()-30)
-    .height(60)
-    .margins({top: 10, left:5, right: 10, bottom:-1})
-    .colors(singleColor)
-    .group(status.group())
-    .gap(1)
-    .dimension(status)
-    .elasticX(true)
-    .xAxis().ticks(0);
+      dataCount
+      .dimension(index)
+      .group(all);
 
-  sourceChart
-    .width($('#source-chart').innerWidth()-30)
-    .height(158)
-    .margins({top: 10, left:5, right: 10, bottom:-1})
-    .colors(singleColor)
-    .group(sources.group())
-    .dimension(sources)
-    .elasticX(true)
-    .gap(1)
-    .ordering(function(i){return -i.value;})
-    .xAxis().ticks(0);
+      statusChart
+      .width($('#status-chart').innerWidth()-30)
+      .height(60)
+      .margins({top: 10, left:5, right: 10, bottom:-1})
+      .colors(singleColor)
+      .dimension(are_closed)
+      .group(are_closed.group())
+      .gap(1)
+      .elasticX(true)
+      .xAxis().ticks(0);
 
-  neighborhoodChart
-    .width($('#neighborhood-chart').innerWidth()-30)
-    .height(435)
-    .margins({top: 10, left:5, right: 10, bottom:20})
-    .colors(singleColor)
-    .group(neighborhoods.group())
-    .dimension(neighborhoods)
-    .elasticX(true)
-    .gap(1)
-    .ordering(function(i){return -i.value;})
-    .labelOffsetY(12)
-    .xAxis().ticks(3);
+      
 
-  reasonChart
-    .width($('#reason-chart').innerWidth()-30)
-    .height(1000)
-    .margins({top: 10, left:5, right: 10, bottom:20})
-    .colors(singleColor)
-    .group(reasons.group())
-    .dimension(reasons)
-    .elasticX(true)
-    .gap(1)
-    .ordering(function(i){return -i.value;})
-    .labelOffsetY(12)
-    .xAxis().ticks(3);
+      serviceTypeChart
+      .width($('#serviceType-chart').innerWidth()-30)
+      .height(435)
+      .margins({top: 10, left:5, right: 10, bottom:20})
+      .colors(singleColor)
+      .group(service_type.group())
+      .gap(1)
+      .dimension(service_type)
+      .elasticX(true)
+      .ordering(function(i) { return -i.value; })
+      .labelOffsetY(12)
+      .xAxis().ticks(3)
+      
+      serviceTypeChart.on("postRedraw", onFiltered);
 
-  openDaysChart
-    .width($('#opendays-chart').innerWidth()-30)
-    .height(533)
-    .margins({top: 10, left:5, right: 10, bottom:20})
-    .colors(singleColor)
-    .group(days_open.group())
-    .dimension(days_open)
-    .elasticX(true)
-    .gap(1)
-    .labelOffsetY(12)
-    .xAxis().ticks(3);
+      openDaysChart
+      .width($('#opendays-chart').innerWidth()-30)
+      .height(533)
+      .margins({top: 10, left:5, right: 10, bottom:20})
+      .colors(singleColor)
+      .group(days_open.group())
+      .dimension(days_open)
+      .elasticX(true)
+      .gap(1)
+      .label(function(d) { return ""; })
+      //.labelOffsetY(12)
+      .ordering(function(i) { return -i.value; })
+      .xAxis().ticks(3);
 
-  dataTable
-    .dimension(open_dates)
-    .group(function (d) { 
-      return tda_date + " &ndash; present";
-    })
-    .size(100) // (optional) max number of records to be shown, :default = 25
-    .columns([
-      function(d) { return d.d; },
-      function(d) { return d.id; },
-      function(d) { return d.a; },
-      function(d) { return d.t; },
-      function(d) { return d.l; },
-      function(d) { return d.s; },
-      function(d) { return d.c_exp; }
-    ])
-    .sortBy( function(d) { return d.d })
-    .order(d3.descending); 
+      dataTable
+      .dimension(open_dates)
+      .group(function (d) { 
+        return tda_date + " &ndash; present";
+      })
+      .size(100) // (optional) max number of records to be shown, :default = 25
+      .columns([
+              function(d) { return d.date_opened; },
+              function(d) { return d.Closed; },
+              function(d) { return d.days_open; },
+              // function(d) { return d.t; },
+              // function(d) { return d.l; },
+              // function(d) { return d.s; },
+              // function(d) { return d.c_exp; }
+      ])
+      .sortBy( function(d) { return d.days_open })
+      .order(d3.descending); 
 
-  dc.renderAll();
-  updateMap(locations.top(Infinity));
+      dc.renderAll();
+      updateMap(locations.top(Infinity));
 
-});
+    }})
+}
 
 window.onresize = function(event) {
   allCharts.forEach(function(chart) {
